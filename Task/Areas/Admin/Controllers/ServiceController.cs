1bfcbb237feed4bedf26a7aae204e7b6f9c45ed1
Sahinshf <shahinrsha@code.edu.az>
2023-04-30 03:31:33 +0400

    Task is added

---
+++ Task/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Task.Contexts;
+
+namespace Task.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public ServiceController(AppDbContext context)
+        {
+            _context= context;  
+        }
+        
+        public IActionResult Index()
+        {
+            List<Service> services = _context.Services.ToList();    
+         
+            ViewBag.count = services.Count; 
+            
+            return View(services);
+        }
+
+
+        public IActionResult Create() { 
+
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Create(Service service)
+        {
+            _context.Services.Add(service); 
+            _context.SaveChanges();
+
+            return View();
+        }
+
+        public IActionResult Detail(int id) { 
+        
+            //_context.Services.Find(id);
+            Service? service = _context.Services.FirstOrDefault(s => s.Id == id);
+            if (service is null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        public IActionResult Delete(int id) {
+
+            if (_context.Services.Count()==1)
+            {
+                return BadRequest();
+            }
+
+            Service? service = _context.Services.FirstOrDefault(s => s.Id == id);
+            if (service is null)
+            {
+                return NotFound();
+            }
+
+            return  View(service);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteService(int id)
+        {
+
+            Service? service = _context.Services.FirstOrDefault(s => s.Id == id);
+            if (service is null)
+            {
+                return NotFound();
+            }
+
+            _context.Services.Remove(service);
+            _context.SaveChanges();
+
+            return View(service);
+        }
+
+        public IActionResult Update(int id)
+        {
+            Service? service = _context.Services.FirstOrDefault(s => s.Id == id);
+            if (service is null)
+            {
+                return NotFound();
+            }
+
+
+            return View(service);
+
+        }
+
+        [HttpPost]
+        public IActionResult Update(Service serviceItem , int id)
+        {
+
+            // First Method
+            //Service? service = _context.Services.FirstOrDefault(s => s.Id == id);
+            //if (service is null)
+            //{
+            //    return NotFound();
+            //}
+
+            //service.Name = serviceItem.Name;    
+            //service.Description = serviceItem.Description;  
+            //service.Image = serviceItem.Image;
+
+            //Second way
+            Service? service = _context.Services.AsNoTracking().FirstOrDefault(s => s.Id == id);
+            if (service is null)
+            {
+                return NotFound();
+            }
+            _context.Services.Update(serviceItem);
+
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
---
+++ Task/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task.Contexts;
+
+namespace Task.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;       
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Slide> slides = _context.Slides.ToList();
+
+            return View(slides);
+        }
+
+        public IActionResult Create() {
+        
+            return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Create(Slide slide)
+        {
+
+            if (!ModelState.IsValid) // Check annotation is valid
+            {
+                return View();
+            }
+
+            if (slide.Offer >100)
+            {
+                ModelState.AddModelError("Offer", "More than 100");
+                return View(); 
+
+            }
+
+            _context.Slides.Add(slide); // Add slide`ı to table
+            _context.SaveChanges(); // Save table to Database
+
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
